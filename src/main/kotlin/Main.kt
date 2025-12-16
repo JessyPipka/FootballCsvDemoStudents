85fdaec665cad87c83088f97c556f6bf277284bf
Konoplev Roman <96744135+JessyPipka@users.noreply.github.com>
2025-12-16 18:30:12 +0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
-fun main(args: Array<String>) {
-    print("Yeah rock!")
+import parser.CsvParser
+import resolver.IResolver
+import resolver.Resolver
+
+fun main() {
+    val players = CsvParser.parsePlayers()
+    val resolver: IResolver = Resolver()
+
+    println("Players without agency: ${resolver.countWithoutAgency(players)}")
+    resolver.bestScorerDefender(players)?.let { (name, goals) ->
+        println("Best defender scorer: $name with $goals goals")
+    }
+    resolver.expensiveGermanPlayerPosition(players)?.let { position ->
+        println("Most expensive German player position: $position")
+    }
+    resolver.rudestTeam(players)?.let { team ->
+        println("Rudest team: ${team.name} (${team.city})")
+    }
 }
