9f029be728bfd2fe24d6f23e041da6ad5f6ec93b
Konoplev Roman <96744135+JessyPipka@users.noreply.github.com>
2025-12-16 18:32:09 +0500

    Update Resolver.kt

--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,31 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Position
+import model.Team
+
+class Resolver : IResolver {
+
+    override fun countWithoutAgency(players: List<Player>): Int =
+        players.filter { it.agency.isNullOrBlank() }.count()
+
+    override fun bestScorerDefender(players: List<Player>): Pair<String, Int>? =
+        players
+            .filter { it.position == Position.DEFENDER }
+            .maxByOrNull { it.goals }
+            ?.let { it.name to it.goals }
+
+    override fun expensiveGermanPlayerPosition(players: List<Player>): String? =
+        players
+            .filter { it.nationality.equals("Germany", ignoreCase = true) }
+            .maxByOrNull { it.transferCost }
+            ?.position
+            ?.russianName
+
+    override fun rudestTeam(players: List<Player>): Team? =
+        players
+            .groupBy { it.team to it.city }
+            .mapValues { (_, members) -> members.map { player -> player.redCards }.average() }
+            .maxByOrNull { it.value }
+            ?.let { (key, _) -> Team(name = key.first, city = key.second) }
+}
